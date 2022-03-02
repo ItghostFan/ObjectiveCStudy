6ed052d22e4ce46229461be1b821046031a49da2
fanchunxing1 <fanchunxing1@joyy.com>
2022-03-02 11:04:10 +0800

    创建工程

---
+++ ObjectiveCStudy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjectiveCStudy
+//
+//  Created by ItghostFan on 2022/3/2.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjectiveCStudy/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ObjectiveCStudy
+//
+//  Created by ItghostFan on 2022/3/2.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
